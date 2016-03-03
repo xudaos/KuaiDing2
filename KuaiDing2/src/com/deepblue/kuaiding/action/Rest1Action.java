461bc682da8771ff6eabbc1f6d911590eb6e8b77
wuting <wuting@withub.net.cn>
2016-03-03 15:29:27 +0800

    wuting 2016-03-03 15:28 添加rest1界面以及图片、css修改等。

---
+++ KuaiDing2/src/com/deepblue/kuaiding/action/Rest1Action.java
@@ -0,0 +1,24 @@
+package com.deepblue.kuaiding.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.utility.json.JsonFeedback;
+import com.utility.json.iJsonable;
+
+public class Rest1Action extends ActionSupport implements iJsonable{
+	private static final long serialVersionUID = 1L;
+	private JsonFeedback feedback = new JsonFeedback();
+	public JsonFeedback getJsonObject() {
+		return feedback;
+	}
+	
+	
+	public String rest1 () {
+		return SUCCESS;
+	}
+	
+
+	/* get set
+	 * ----------------------------------------------------------
+	 * */
+	
+}
